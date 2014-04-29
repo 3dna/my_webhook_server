6b3e146e33322624884b7c012bc7dc24841f3456
Ray Pereda <ray@nationbuilder.com>
2014-04-29 13:21:51 -0700

    Add json processing

--- php/my_app.php
+++ php/my_app.php
@@ -1,3 +1,16 @@
 <?php
-  echo $_POST["email"];
+$json_data = file_get_contents("php://input");
+
+$person = json_decode($json_data, true);
+
+// var_dump($person);
+
+// echo "==============================="
+
+// echo $person;
+
+echo $person['email'];
+
+// echo "hello";
+
 ?>
